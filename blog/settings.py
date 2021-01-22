ca0da30e15f4a0d7102a77aba33191e61ce19eb6
juusoviita <juusoviita@gmail.com>
2021-01-22 14:51:48 +0200

    set up the basic structure and styling

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogsite.apps.BlogsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
